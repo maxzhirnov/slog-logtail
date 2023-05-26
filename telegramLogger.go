286a787837cdd66cd2dd0c1d1d34b7e1b66cb3dc
mzhirnov <maximz2009@gmail.com>
2023-05-26 15:30:32 +0300

    refactoring

--- telegramLogger.go
+++ telegramLogger.go
@@ -7,29 +7,33 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type TelegramLoger struct {
+type TelegramLogger interface {
+	Log(logLevel LogLevel, msgName string, body ...any)
+}
+
+type tgLoger struct {
 	Token  string
 	ChatID int64
 	Logger *Logger
 }
 
-func NewTelegramLoger(token string, chatID int64, logger *Logger) *TelegramLoger {
-	return &TelegramLoger{
+func NewTelegramLoger(token string, chatID int64, logger *Logger) TelegramLogger {
+	return &tgLoger{
 		Token:  token,
 		ChatID: chatID,
 		Logger: logger,
 	}
 }
 
-func (tl TelegramLoger) Log(logLevel LogLevel, msgName string, body ...any) {
-	go func() { tl.Logger.Log(logLevel, msgName, body...) }()
-	bot, err := tgbotapi.NewBotAPI(tl.Token)
+func (l tgLoger) Log(logLevel LogLevel, msgName string, body ...any) {
+	go func() { l.Logger.Log(logLevel, msgName, body...) }()
+	bot, err := tgbotapi.NewBotAPI(l.Token)
 	if err != nil {
 		log.Println(err)
 	}
 	bot.Debug = false
 
-	msg := tgbotapi.NewMessage(tl.ChatID, fmt.Sprintf("[[%s]]\n%s\n%s", logLevel, msgName, body))
+	msg := tgbotapi.NewMessage(l.ChatID, fmt.Sprintf("[[%s]]\n%s\n%s", logLevel, msgName, body))
 	msg.ParseMode = "Markdown"
 	if _, err := bot.Send(msg); err != nil {
 		log.Println(err)
