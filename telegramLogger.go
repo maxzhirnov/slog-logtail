898a08c21e84a5c642dc1c1dabdc4d45f10db0c8
mzhirnov <maximz2009@gmail.com>
2023-05-26 16:05:24 +0300

    refactoring

--- telegramLogger.go
+++ telegramLogger.go
@@ -11,29 +11,29 @@ type TelegramLogger interface {
 	Log(logLevel LogLevel, msgName string, body ...any)
 }
 
-type tgLoger struct {
+type TelegramLogerObject struct {
 	Token  string
 	ChatID int64
 	Logger *Logger
 }
 
 func NewTelegramLoger(token string, chatID int64, logger *Logger) TelegramLogger {
-	return &tgLoger{
+	return &TelegramLogerObject{
 		Token:  token,
 		ChatID: chatID,
 		Logger: logger,
 	}
 }
 
-func (l tgLoger) Log(logLevel LogLevel, msgName string, body ...any) {
-	go func() { l.Logger.Log(logLevel, msgName, body...) }()
-	bot, err := tgbotapi.NewBotAPI(l.Token)
+func (tlo TelegramLogerObject) Log(logLevel LogLevel, msgName string, body ...any) {
+	tlo.Logger.Log(logLevel, msgName, body...)
+	bot, err := tgbotapi.NewBotAPI(tlo.Token)
 	if err != nil {
 		log.Println(err)
 	}
 	bot.Debug = false
 
-	msg := tgbotapi.NewMessage(l.ChatID, fmt.Sprintf("[[%s]]\n%s\n%s", logLevel, msgName, body))
+	msg := tgbotapi.NewMessage(tlo.ChatID, fmt.Sprintf("[[%s]]\n%s\n%s", logLevel, msgName, body))
 	msg.ParseMode = "Markdown"
 	if _, err := bot.Send(msg); err != nil {
 		log.Println(err)
